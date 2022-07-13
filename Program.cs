581691382512fd082ecf1e5608c53be7f52d50a0
ProfeQuiroz <108696679+ProfeQuiroz@users.noreply.github.com>
2022-07-12 22:15:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRecuperatorioej1QuirozMarcela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Comprar pesos";
+            double dolares;
+            double euros;
+            double oficial;
+            double paralelo;
+
+            Console.Write("Ingrese los dolares disponibles:");
+            dolares = double.Parse(Console.ReadLine());
+
+            oficial= dolares * 230;
+            paralelo= dolares * 280;
+            euros= dolares / 1.17;
+        { 
+            Console.WriteLine($"Puede adquirir {oficial} pesos en el mercado oficial");
+            Console.WriteLine($"Puede adquirir {paralelo} pesos en el mercado paralelo");
+            Console.WriteLine($"Su equivalente en euros es {euros}");
+        }
+            Console.ReadLine();
+
+        }
+        
+    }
+}
